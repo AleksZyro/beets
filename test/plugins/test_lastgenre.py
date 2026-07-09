4b7aaab8521fc36ee88301d3a35dea8483db2f37
Šarūnas Nejus <snejus@protonmail.com>
2026-07-09 12:15:33 +0100

    Normalize psytrance

--- test/plugins/test_lastgenre.py
+++ test/plugins/test_lastgenre.py
@@ -1093,6 +1093,7 @@ class TestAliases:
             ("indie rock", "indie rock"),
             ("juke/footwork", "juke/footwork"),
             ("Funk / Soul", "funk / soul"),
+            ("psy-trance", "psytrance"),
             ("psy rock", "psychedelic rock"),
             ("psych-pop", "psychedelic pop"),
         ],
