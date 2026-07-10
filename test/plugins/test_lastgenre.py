2a44e53899f03c2a93acbf200b2b2d9ab9d1c762
Šarūnas Nejus <snejus@protonmail.com>
2026-07-10 05:18:00 +0100

    Handle psychedelic folk/soul

--- test/plugins/test_lastgenre.py
+++ test/plugins/test_lastgenre.py
@@ -1093,6 +1093,8 @@ class TestAliases:
             ("indie rock", "indie rock"),
             ("juke/footwork", "juke/footwork"),
             ("Funk / Soul", "funk / soul"),
+            ("psy rock", "psychedelic rock"),
+            ("psych-pop", "psychedelic pop"),
         ],
     )
     def test_default_alias_patterns(self, config, input_genre, expected_genre):
